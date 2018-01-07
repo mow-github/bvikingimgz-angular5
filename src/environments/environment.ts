6193bc6c92b3cc9c6bc29e7e68b64cd8aeb59e49
Mats Wikmar <matwik@gmail.com>
2018-01-07 06:43:50 +0100

    Fix #5, added env firebase data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,14 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAdC7zXa-O3i-9kea7K_OAnDv426zd4UbU',
+    authDomain: 'bvikingogram.firebaseapp.com',
+    databaseURL: 'https://bvikingogram.firebaseio.com',
+    projectId: 'bvikingogram',
+    storageBucket: 'bvikingogram.appspot.com',
+    messagingSenderId: '550884347336'
+  },
 };
